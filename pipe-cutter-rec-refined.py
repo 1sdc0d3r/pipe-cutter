53e26630b30cde789a182eeb91fc16610e5e8074
Braden Bell <fish0859@gmail.com>
2022-05-21 17:37:20 -0600

    psudocode for possible way to find best combo

--- pipe-cutter-rec-refined.py
+++ pipe-cutter-rec-refined.py
@@ -14,7 +14,7 @@ def get_cuts(pipe_cuts_dic):
         for i in range(pipe_cuts_dic[pipe]):
             needed_cuts.append(pipe)
             pipe_cuts_dic[pipe] -= 1
-    return set(needed_cuts)
+    return needed_cuts
 
 
 pipe_cuts = get_cuts(dict(pipe_cuts_dict))
@@ -96,24 +96,23 @@ all_valid_combos = valid_combo_reducer(
 # From the all_valid_combos can I find combos with least amount of waste. See what combos of combos would result in the min amount of pipe?
 
 
-def leftover_pipe(combo_list):
+def leftover_pipe_length(combo_list):
     cut_dict = pipe_cuts_dict.copy()
-    combo_list_leftover_count = list()
-    print(combo_list)
+
     for combo in combo_list:
         for x in combo:
             if cut_dict[x] > 0:
                 cut_dict[x] -= 1
-    #         else:
-    #             # recursive go through each list for least pipe leftover ?? sum(combo)
-    #             # print(False)
-    #             pass  # ! temp pass
+            else:
+                # just in case a invalid combo gets sent in - bugs happen (do I need to return if a combo doesn't work? validator should have caught it if not)
+                pass
+    return sum(get_cuts(cut_dict))
 
-    # for combo in combo_list:
-    #     print(sum(combo))
+# remove all cuts in combos from dictionary copy
+# use sum(getCuts())
 
 
-# leftover_pipe(all_valid_combos)
+# leftover_pipe_length([[3, 3, 3], [3, 3, 3, 3, 3, 5, 15]]) # 79"
 
 
 # use perfect combo's first?
@@ -131,15 +130,33 @@ def combo_still_valid(cut_dict, combo):
             return False
     return True
 
+# * check leftover pipe length from list of combos
+
 
 def find_best_combo_of_combos(cut_dict, combo_list):
-    cut_dict_copy = cut_dict.copy()
-    lowest_combo = list()
-    lowest_leftover_pipe = None
+    lowest_combo_list = []
+    lowest_pipe_length = None
+
+    cur_pipe_cuts_needed = cut_dict.copy()
+    cur_combo_list = []
 
     for combo in combo_list:
-        if not combo_still_valid(cut_dict_copy, combo):
+        if not combo_still_valid(cur_pipe_cuts_needed, combo):
             pass
+
+
+# check combo if still valid
+# add combo to cur_combo_list
+# remove cuts in combo1 from pipe_cuts_needed
+# check cur_pipe_cuts_needed for any cuts left
+    # if so repeat check-add-remove
+    # if not check leftover_pipe_length()
+        # if new length < lowest_pipe_length
+            # lowest_combo_list becomes cur_combo_list
+            # set lowest_pipe_length
+
+
+# what about combo1 maxing out, then combo 2 until 0 cuts needed left, then pop off last one and find next combo then pop 2 off all the way to empty then start on combo2 maxing out??
 
 
 find_best_combo_of_combos(pipe_cuts_dict, all_valid_combos)
@@ -149,3 +166,30 @@ find_best_combo_of_combos(pipe_cuts_dict, all_valid_combos)
 # check each combo-list for leftover cuts. If one is found replace lowest combo variable. Is matrix needed then? check each combo of combos for leftover pipe
 
 # recursive find_best_combo_of_combos?
+
+# variable pipe lengths? black iron milled 18-22ft
+
+
+# def all_perfect_combination_rec(target, current_sum, start, output, result):
+#     if current_sum == target:
+#         output.append(result.copy())
+
+#     for i in pipe_cuts:
+#         temp_sum = current_sum + i
+#         if temp_sum <= target:
+#             result.append(i)
+#             all_perfect_combination_rec(target, temp_sum, i, output, result)
+#             # !!!!!!!!!!!!!!! This is not needed for perfect_combo_rec
+#             # print(result)  # This result variable is all possible combinations
+#             # print("TEST", [x for x in result])
+#             # result.sort()
+#             result_copy = result.copy()
+#             result_copy.sort()
+
+#             if result_copy not in all_combos:
+#                 all_combos.append(result_copy)
+#             # !!!!!!!!!!!!!!!!
+
+#             result.pop()
+#         else:
+#             return
